98886e17989eba69c194fe9eed072c5151bf251a
me <s932172@aics.espritscholen.nl>
2025-11-22 20:08:09 +0100

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -1,93 +1,122 @@
-import cv2
-import os
-import numpy as np
-
-
-folder_path = '/Users/s932172@aics.espritscholen.nl/Desktop/game development/cv development'
-os.chdir(folder_path)
-
-video = cv2.VideoCapture('invisibility_video.mov')
-
-
-bg_frames = []
-num_bg_frames = 50
-
-for i in range(num_bg_frames):
-    ret, frame = video.read()
-    if not ret:
-        print('Frame could not be read')
-        continue
-    bg_frames.append(frame)
-
-if len(bg_frames) == 0:
-    print("No background")
-    exit()
-
-background = np.median(bg_frames, axis=0).astype(np.uint8)
-background = np.flip(background, axis=1)
-
-green_low = np.array([30, 30, 15])
-green_top = np.array([100, 255, 200])
-
-kernel = np.ones((7, 7), np.uint8)
-
-
-while video.isOpened():
-    # frame_is, frame = video.read()
-    # if not frame_is:
-    #     break
-
-    # frame = np.flip(frame, axis=1)
-    # frame = cv2.GaussianBlur(frame, (3, 3), 0)
-    # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    # mask = cv2.inRange(hsv, green_low, green_top)
-
-    # mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
-    # mask = cv2.medianBlur(mask, 7)
-    # mask = cv2.GaussianBlur(mask, (7,7), 0)
-    
-    # mask_inv = cv2.bitwise_not(mask)
-    
-    # cloak_area = cv2.bitwise_and(background, background, mask=mask)
-    # not_cloak_area = cv2.bitwise_and(frame, frame, mask=mask_inv)
-
-    
-    
+class binary_search_node():
+    def __init__(self, key=None, value=None, left_node=None, right_node=None):
+        self.key = key
+        self.value = value
+        self.left_node:binary_search_node = left_node
+        self.right_node:binary_search_node = right_node
         
-    # final_frame = cv2.addWeighted(cloak_area, 1, not_cloak_area, 1, 0)
+        
+        
+    def InOrderTraversal(self):
+        current_list = []
+        if self.left_node != None:
+            current_list.extend(self.left_node.InOrderTraversal())
+        current_list.append((self.key, self.value))
+        if self.right_node != None:
+            current_list.extend(self.right_node.InOrderTraversal())
+        
+        return current_list
     
+    def PreOrderTraversal(self):
+        current_list = [(self.key, self.value)]
+        if self.left_node != None:
+            current_list.extend(self.left_node.InOrderTraversal())
+        if self.right_node != None:
+            current_list.extend(self.right_node.InOrderTraversal())
+        
+        return current_list
     
+    def PostOrderTraversal(self):
+        current_list = []
+        if self.left_node != None:
+            current_list.extend(self.left_node.InOrderTraversal())
+        if self.right_node != None:
+            current_list.extend(self.right_node.InOrderTraversal())
+            
+        current_list.append((self.key, self.value))
+        
+        return current_list
+
+        
+        
     
+    def insert(self, key, value):
+        if self.key == None:
+            self.key = key
+            self.value = value
+            return
+        if key < self.key:
+            if self.left_node == None:
+                self.left_node = binary_search_node(key, value)
+            else:
+                self.left_node.insert(key, value)
+        else:
+            if self.right_node == None:
+                self.right_node = binary_search_node(key, value)
+            else:
+                self.right_node.insert(key, value)
     
 
-    # cv2.imshow('invisibility cloak', final_frame)
-
-    # key = cv2.waitKey(5)
-    # if key == 27:
-    #     break
-    
-    
-    is_frame, frame = video.read()
-    if not is_frame:
-        print('cannot read frame to flip')
-        break
-    frame = np.flip(frame, axis=1)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    final_mask = cv2.inRange(hsv, green_low, green_top)
-    final_mask = cv2.morphologyEx(final_mask, cv2.MORPH_OPEN, kernel, iterations=1)
-    
-    inverted = cv2.bitwise_not(final_mask)
-    
-    red_colors = cv2.bitwise_and(background, background, mask=final_mask)
-    not_red_colors = cv2.bitwise_and(frame, frame, mask=inverted)
-    
-    final_frame = cv2.addWeighted(red_colors, 1, not_red_colors, 1, 0)
-    cv2.imshow('invisible image', final_frame)
-    x = cv2.waitKey(5)
-    if x == 27:
-        break
+    def find(self, key):
+        if key == self.key:
+            return self.value
+        elif key < self.key:
+            if self.left_node != None:
+                return self.left_node.find(key)
+            else:
+                return False
+        else:
+            if self.right_node != None:
+                return self.right_node.find(key)
+            else:
+                return False
 
 
-video.release()
-cv2.destroyAllWindows()
+# root = binary_search_node(5, 'a')
+    
+    
+# for i in range(5):
+#     key = int(input('Enter a key: '))
+#     value = input('Enter a value: ')
+#     root.insert(key, value)
+    
+    
+# print(root.InOrderTraversal())
+    
+# print(root.find(8))
+
+string = """
+Actions
+1. create tree
+2. insert key & value
+3. print inordertraversal
+4. print preordertraversal
+5. print postordertraversal
+6. find value from key
+7. quit"""
+
+
+
+while True:
+    print(string)
+    action = int(input('Enter action number: '))
+    if action == 1:
+        root = binary_search_node()
+        print('created tree')
+    elif action == 2:
+        key = int(input('Enter a key: '))
+        value = input('Enter a value: ')
+        root.insert(key, value)
+    elif action == 3:
+        print(root.InOrderTraversal())
+    elif action == 4:
+        print(root.PreOrderTraversal())
+    elif action == 5:
+        print(root.PostOrderTraversal())
+    elif action == 6:
+        key == int(input("Enter key: "))
+        print(root.find(key))
+    elif action == 7:
+        print('exiting')
+        exit()
+    
