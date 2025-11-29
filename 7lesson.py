d075f378cb6b5c3d2b4eace77e71df10b84e859f
me <s932172@aics.espritscholen.nl>
2025-11-29 20:48:43 +0100

    Add files via upload

--- 7lesson.py
+++ 7lesson.py
@@ -1,40 +1,142 @@
-import cv2
-import os
-
-
-car_video_path = '/Users/s932172@aics.espritscholen.nl/Desktop/game development/cv development/cars1.mp4'
-
-
-car_video = cv2.VideoCapture(car_video_path)
-
-
-cars_classifier_path = '/Users/s932172@aics.espritscholen.nl/Desktop/game development/cv development/cars.xml'
-
-cars_classifier = cv2.CascadeClassifier(cars_classifier_path)
-
-is_frame = True
-
-while is_frame:
-    is_frame, frame = car_video.read()
-    if not is_frame:
-        print('no frame')
+import random
+class queue():
+    def __init__(self, max_elements):
+        self.list = list()
+        self.max_elements = max_elements
         
-    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cars = cars_classifier.detectMultiScale(grayscale, 1.07, 4)
-    for x, y, width, height in cars:
-        cv2.rectangle(frame, (x, y), (x+width, y+width), [255, 0, 0], 3)
+    def enqueue(self, item):
+        if len(self.list) < self.max_elements:
+            self.list.append(item)
+        else:
+            raise Exception(f'queue overflow. queue maximum height exceeded ({self.max_elements})')
         
-    num_cars = len(cars)
+            
+    def dqueue(self):
+        if len(self.list) > 0:
+            return self.list.pop(0)
+        else:
+            raise Exception('queue is empty. queue has 0 elements.')
+        
+    def peek(self):
+        if len(self.list) > 0:
+            return self.list[0]
+        else:
+            raise Exception('queue is empty. queue has 0 elements.')
+        
+    def get_rear(self):
+        if len(self.list) > 0:
+            return self.list[-1]
+        else:
+            raise Exception('queue is empty. queue has 0 elements.')
+        
+    def show_full(self):
+        return self.list
     
-    frame = cv2.putText(frame, f'num cars: {num_cars}', (300, 50), cv2.FONT_ITALIC, 2, (255, 0, 0), 2, cv2.LINE_AA)
     
-    cv2.imshow('video', frame)
+    def __len__(self):
+        return len(self.list)
     
-    key = cv2.waitKey(5)
-    if key == 27:
-        break
+    def __str__(self):
+        return str(self.list)
     
+    
+class stack():
+    def __init__(self, max_elements):
+        self.list = list()
+        self.max_elements = max_elements
+        
+    def add(self, item):
+        if len(self.list) < self.max_elements:
+            self.list.append(item)
+        else:
+            raise Exception(f'Stack overflow. Stack maximum height exceeded ({self.max_elements})')
+            
+    def take(self):
+        if len(self.list) > 0:
+            return self.list.pop(-1)
+        else:
+            raise Exception('Stack is empty. Stack has 0 elements.')
+        
+    def top(self):
+        if len(self.list) > 0:
+            return self.list[-1]
+        else:
+            raise Exception('Stack is empty. Stack has 0 elements.')
+        
+    def show_full(self):
+        return self.list
+    
+    def clear(self):
+        self.list.clear()
+        
+    def __len__(self):
+        return len(self.list)
+    
+    def __str__(self):
+        return str(self.list)
+    def __repr__(self):
+        return str(self.list)
+    
+    
+    
+# people = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# people_queue = queue(100)
+# for person in people:
+#     people_queue.enqueue(person)
     
 
+# while len(people_queue) > 1:
+#     max_time = random.randint(0, 100)
+
+#     for i in range(max_time):
+#         people_queue.enqueue(people_queue.dqueue())
+
+#     print(people_queue.dqueue(), 'has the potato!')
     
-cv2.destroyAllWindows()
+    
+# print(people_queue.peek(), 'has won!')
+
+    
+
+length = 100
+
+tower1 = stack(100)
+for i in reversed(range(1, length+1)):
+    tower1.add(i)
+
+towers = [tower1]
+
+for i in range(length-1):
+    towers.append(stack(100))
+
+
+# def move_blocks(towers:list[stack]):
+#     for i in range(1, len(towers)):
+#         towers[i].add(towers[0].take())
+#     towers[-2].add(towers[-1].take())
+#     towers[-1].add(towers[0].take())
+#     towers[0].add(towers[-2].take())
+#     for i in reversed(range(len(towers)-1, 1, -1)):
+#         towers[-1].add(towers[i].take())
+#     towers[-1].add(towers[0].take())
+    
+    # return towers
+        
+        
+def move_blocks(towers:list[stack]):
+    for i in reversed(range(1, len(towers))):
+        towers[i].add(towers[0].take())
+        
+    towers[-2].add(towers[-1].take())
+    towers[-1].add(towers[0].take())
+    towers[0].add(towers[-2].take())
+    for i in range(1, len(towers)-1):
+        towers[-1].add(towers[i].take())
+        
+    towers[-1].add(towers[0].take())
+    
+    return towers
+        
+
+
+print(move_blocks(towers))
