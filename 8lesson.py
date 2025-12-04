b89205c0e2e856df200e2fe0a36d86433503e54b
me <s932172@aics.espritscholen.nl>
2025-12-04 19:30:26 +0100

    Add files via upload

---
+++ 8lesson.py
@@ -0,0 +1,89 @@
+class queue():
+    def __init__(self, max_elements):
+        self.list = list()
+        self.max_elements = max_elements
+        
+    def enqueue(self, item):
+        if len(self.list) < self.max_elements:
+            self.list.append(item)
+        else:
+            raise Exception(f'queue overflow. queue maximum height exceeded ({self.max_elements})')
+        
+            
+    def dqueue(self):
+        if len(self.list) > 0:
+            return self.list.pop(0)
+        else:
+            raise Exception('queue is empty. queue has 0 elements.')
+        
+    def peek(self):
+        if len(self.list) > 0:
+            return self.list[0]
+        else:
+            raise Exception('queue is empty. queue has 0 elements.')
+        
+    def get_rear(self):
+        if len(self.list) > 0:
+            return self.list[-1]
+        else:
+            raise Exception('queue is empty. queue has 0 elements.')
+        
+    def show_full(self):
+        return self.list
+    
+    
+    def __len__(self):
+        return len(self.list)
+    
+    def __str__(self):
+        return str(self.list)
+    
+    
+class graph():
+    def __init__(self, vertices:list=[], connections=None, edges=None):
+        self.vertices = vertices
+        if connections is not None:
+            self.connections = connections
+        elif edges is not None:
+            self.connections = {}
+            for start, end in edges:
+                if start in self.connections:
+                    self.connections[start].append(end)
+                else:
+                    self.connections[start] = [end]
+                    
+                if end in self.connections:
+                    self.connections[end].append(start)
+                else:
+                    self.connections[end] = [start]
+                    
+        
+        
+    def BFS(self, root=None, queue_max=100):
+        if root is None:
+            root = self.vertices[0]
+            
+        node_queue = queue(queue_max)
+        visited = []
+        node_queue.enqueue(root)
+        while len(node_queue) > 0:
+            first_node = node_queue.dqueue()
+            if first_node not in visited:
+                visited.append(first_node)
+                
+                for connected_node in self.connections[first_node]:
+                    node_queue.enqueue(connected_node)
+                    
+        return visited
+    
+    
+
+vertices = [1,2,3,4,5,6,7]
+# connections = {1:[2, 3], 2:[5, 4, 1], 3:[1, 5], 4:[2], 5:[2, 3]}
+
+edges = [(1,2), (1,3), (1, 4), (2,5), (5,6), (5,7)]
+                
+this_graph = graph(vertices, edges=edges)
+
+
+print(this_graph.BFS())
