f9dcd9c7a0158da9b78653cc25be6e17a635a4ca
me <s932172@aics.espritscholen.nl>
2026-01-29 19:47:56 +0100

    Add files via upload

--- hw6_2.py
+++ hw6_2.py
@@ -10,17 +10,17 @@ os.chdir('/Users/s932172@aics.espritscholen.nl/Desktop/game development/data sci
 data = pd.read_csv('titanic.csv')[['Survived','Pclass','Sex','Age','Fare']]
 
 import sklearn
-print(sklearn.__version__)
+# print(sklearn.__version__)
 
 # input and output
 
-input = data.iloc[:, 0:-1] #all columns except last
-output = data.iloc[:, -1]
+input = data.iloc[:, 1:] #all columns except last
+output = data.iloc[:, 0]
 
 # input['Sex'].replace(to_replace='male',value=1)
 # input['Sex'].replace(to_replace='female',value=0)
 input['Sex'] = input['Sex'].replace({'male':1, 'female':0})
-print(input.head())
+# print(input.head())
 
 
 # #categorical to numerical input
@@ -32,7 +32,7 @@ print(input.head())
 # input = first.fit_transform(input)
 # input = second.fit_transform(input)
 
-print(input)
+# print(input)
 
 
 # from sklearn.preprocessing import LabelEncoder
@@ -46,7 +46,7 @@ print(input)
 from sklearn.model_selection import train_test_split
 
 train_x,test_x,train_y,test_y = train_test_split(input,output,test_size=0.2,random_state=1)
-print(train_x,test_x,train_y,test_y)
+# print(train_x,test_x,train_y,test_y)
 
 
 # from sklearn.preprocessing import StandardScaler
@@ -66,25 +66,26 @@ print(train_x,test_x,train_y,test_y)
 # test_x = pd.DataFrame(test_x)
 # test_y = pd.DataFrame(test_y)
 
+alpha = 0.2
 
 import matplotlib.pyplot as plt
 plt.subplot(221)
-plt.scatter(train_x.iloc[:,0],train_y)
+plt.scatter(train_x.iloc[:,0],train_y,alpha=alpha)
 
 plt.subplot(222)
-plt.scatter(train_x.iloc[:,1],train_y)
+plt.scatter(train_x.iloc[:,1],train_y,alpha=alpha)
 
 plt.subplot(223)
-plt.scatter(train_x.iloc[:,2],train_y)
+plt.scatter(train_x.iloc[:,2],train_y,alpha=alpha)
 
 plt.subplot(224)
-plt.scatter(train_x.iloc[:,3],train_y)
+plt.scatter(train_x.iloc[:,3],train_y,alpha=alpha)
 plt.show()
 
-print('trainx------------------------------------')
-print(train_x)
-print('trainy------------------------------------')
-print(train_y)
+# print('trainx------------------------------------')
+# print(train_x)
+# print('trainy------------------------------------')
+# print(train_y)
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import DecisionTreeRegressor
@@ -99,24 +100,15 @@ test_y:pd.DataFrame = test_y.round(0)
 tree = DecisionTreeClassifier(max_depth=5,random_state=1)
 tree.fit(train_x,train_y)
 prediction = tree.predict(test_x)
-print(prediction)
-print(test_y)
+# print(prediction)
+# print(test_y)
 
 from sklearn import metrics
 
 accuracy = metrics.accuracy_score(test_y,prediction)
 print(accuracy)
 
-print('prediction')
-print(prediction)
-print('testy')
-print(test_y)
-
-total_distance = 0
-
-for pred, real in zip(prediction,test_y):
-    total_distance += abs(pred-real)
-    
-    
-print('average wrongness')
-print(total_distance/len(prediction))
+# print('prediction')
+# print(prediction)
+# print('testy')
+# print(test_y)
