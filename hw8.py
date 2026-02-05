120a19dfe3cbab218c1ba34bbd6e31ae33f564ef
me <s932172@aics.espritscholen.nl>
2026-02-05 19:14:00 +0100

    Add files via upload

--- hw8.py
+++ hw8.py
@@ -20,27 +20,32 @@ deaths_brazil = country_data.nlargest(10, keep='all',columns='Deaths')[['Provinc
 import plotly.graph_objects as go
 
 # india
-confirmed_india_graph = px.bar(confirmed_india, x=confirmed_india['Province_State'], y='Confirmed', color=confirmed_india['Province_State'],title='top 10 states of india by confirmed cases')
-deaths_india_graph = px.bar(deaths_india, x=deaths_india['Province_State'], y='Deaths', color=deaths_india['Province_State'],title='top 10 states of india by death cases')
+confirmed_india_graph = go.Bar(x=confirmed_india['Province_State'], y=confirmed_india['Confirmed'], name='top 10 states of india by confirmed cases')
+deaths_india_graph = go.Bar(x=deaths_india['Province_State'], y=deaths_india['Deaths'], name='top 10 states of india by death cases')
 
-confirmed_india_graph.show()
-deaths_india_graph.show()
+# confirmed_india_graph.show()
+# deaths_india_graph.show()
 
 # brazil
-confirmed_brazil_graph = px.bar(confirmed_brazil, x=confirmed_brazil['Province_State'], y='Confirmed', color=confirmed_brazil['Province_State'],title='top 10 states of brazil by confirmed cases')
-deaths_brazil_graph = px.bar(deaths_brazil, x=deaths_brazil['Province_State'], y='Deaths', color=deaths_brazil['Province_State'],title='top 10 states of brazil by death cases')
+confirmed_brazil_graph = go.Bar(x=confirmed_brazil['Province_State'], y=confirmed_brazil['Confirmed'] ,name='top 10 states of brazil by confirmed cases')
+deaths_brazil_graph = go.Bar(x=deaths_brazil['Province_State'], y=deaths_brazil['Deaths'], name='top 10 states of brazil by death cases')
 
-confirmed_brazil_graph.show()
-deaths_brazil_graph.show()
-
-# print(type(confirmed_brazil_graph))
-# print(type(plt.graph_objs.Bar()))
+# confirmed_brazil_graph.show()
+# deaths_brazil_graph.show()
 
 
-# # confirmed
 # confirmed_graph = go.Figure(data=(confirmed_india_graph,confirmed_brazil_graph))
 # confirmed_graph.show()
 
-# print(type(plt.graph_objs.Bar()))
+
+india_graph = go.Figure(data=(confirmed_india_graph,deaths_india_graph))
+india_graph.show()
+
+
+brazil_graph = go.Figure(data=(confirmed_brazil_graph,deaths_brazil_graph))
+brazil_graph.update_layout(title='confirmed and deaths in states in brazil',barmode='stack')
+brazil_graph.show()
+
+
 
 "homework: also display top states in brazil and india for total cases and deaths"
