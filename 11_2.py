72fe89b961122e1dd3c5bdcb37a9f1fe7844c433
me <s932172@aics.espritscholen.nl>
2026-02-14 21:05:26 +0100

    Add files via upload

---
+++ 11_2.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import plotly.express as px
+import os
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+os.chdir('/Users/s932172@aics.espritscholen.nl/Desktop/game development/data science')
+
+data = pd.read_csv('mpg.csv')
+
+
+
+subplots = make_subplots(rows=2,cols=2,subplot_titles=['horsepower vs mpg',None,None,None])
+
+
+scatter = px.scatter(data,x='horsepower',y='mpg')
+
+for dot in scatter.data:
+    subplots.add_trace(dot,row=1,col=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+subplots.show()
